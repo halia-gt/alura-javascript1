bac2394956dd76702e511fd935aae43b459b82af
Halia <nathalia.gramst@gmail.com>
2022-03-15 17:59:54 -0300

    Teste de template literal com aspas ao invés de crase

---
+++ teste.js
@@ -0,0 +1,7 @@
+const nome = "Nathália";
+const cidade = "Joinville";
+
+const apresentacao = `meu nome é ${nome} e eu moro em ${cidade}`;
+// nao acredito, mas é verdade. O ` `  pra escrever o texto é crase mesmo O_O
+
+console.log(apresentacao);
